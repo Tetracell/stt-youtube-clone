4b2e2ecc9f8f5f7f2ea91a5a5123e82652ac0316
Tom LaTulipe <tlatulipe@gmail.com>
2022-06-14 16:00:47 -0400

    nav bar links

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,13 @@
-const Nav = (() => {
-    return (
-        <p> I am the Nav Bar!</p>
-    )
-})
+import { Link } from "react-router-dom";
+
+const Nav = () => {
+  return (
+    <div>
+      <p> I am the Nav Bar!</p>
+      <Link to={"/"}> Home </Link>
+      <Link to={"/about"}> About</Link>
+    </div>
+  );
+};
 
 export default Nav;
