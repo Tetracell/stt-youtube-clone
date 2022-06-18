70758b1a0c3cf26a0dbfd7ef3a233d4b2151af22
Tom LaTulipe <tlatulipe@gmail.com>
2022-06-18 11:56:24 -0400

    Had to import react on all comps - my fault?

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,3 +1,8 @@
+import React from "react";
+import YouTube from "react-youtube";
+
+
+//Display a sample video in place to make sure functionality works
 const Video = () => {
   return <p>I will be the singular video that you click on!</p>;
 };
