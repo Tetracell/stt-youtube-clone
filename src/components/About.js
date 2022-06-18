b7b1b0a060d40b508bcac142a37efe71c2c9fdc5
Tom LaTulipe <tlatulipe@gmail.com>
2022-06-18 12:06:23 -0400

    Add some stuff for my About

--- src/components/About.js
+++ src/components/About.js
@@ -25,7 +25,9 @@ const About = () => {
             <section className="fellow-three">
                 <img src='https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F594e1b89-fc3b-424c-ad8a-6b05da64c66f%2FTom_LaTulipe.jpg?table=block&id=e74fea20-bb4a-4812-b3bf-3c608dfe5991&spaceId=e2735e9b-8fe9-4c16-bdc1-be142cd1014e&width=2000&userId=508bb533-cfe0-4929-b1f8-94995b637846&cache=v2' alt=""></img>
                 <h3>Tom LaTulipe</h3>
-                <p>This is who I am...</p>
+                <p>I'm an avid pinball player who participates in competition at local and state levels.</p>
+                <p>A lifelong gamer, I primarily play on PC and have been an enthusiast of emulation for almost 30 years.</p>
+                <p>My path in programming has lead me through a variety of languages, including Python, C#, JS</p>
                 <a href="https://github.com/Tetracell">
                 <img src='https://cdn4.iconfinder.com/data/icons/geomicons/32/672344-github-512.png' id="gitlogo" alt='github-logo' className='link'/>
                 </a> 
