8967ca0c233d2993ccccc1923fa52398da95c84f
Tom LaTulipe <tlatulipe@gmail.com>
2022-06-19 15:35:33 -0400

    another cleanup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import { useState } from "react";
 import "./App.css";
 
 // Components
@@ -8,7 +7,7 @@ import Nav from "./components/Nav";
 import Home from "./components/Home";
 import About from "./components/About";
 import Video from "./components/Video";
-import { Error } from "./components/Error";
+
 
 //<Route path="*" element={<Error setIsOpen={setIsOpen}/>} /> Stashed route for now, not sure if
 // going to be neccessary.
