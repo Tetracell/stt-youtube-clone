1bf9da08dad7d26078b8a32bf20049630668614f
Samantha Figueroa <96448061+samfigueroa92@users.noreply.github.com>
2022-06-19 15:16:46 -0400

    add videoshowpage css and changes to videolist

--- src/components/Video.js
+++ src/components/Video.js
@@ -2,9 +2,11 @@ import React from "react";
 import YouTube from "react-youtube";
 import { useParams } from "react-router-dom";
 import Comments from "./Comments";
+import "./VideoShowPage.css"
 
 //Display a sample video in place to make sure functionality works
 const Video = () => {
+  
   let videoId = useParams();
   // console.log(videoId);
   const options = {
