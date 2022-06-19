9bf44428d7aea9bd09c4a4419537533385af1434
Tom LaTulipe <tlatulipe@gmail.com>
2022-06-19 11:58:08 -0400

    Just some garbage

--- src/App.js
+++ src/App.js
@@ -3,18 +3,14 @@ import { Route, Routes } from "react-router-dom";
 import { useState } from "react";
 import "./App.css";
 
-
 // Components
 import Nav from "./components/Nav";
 import Home from "./components/Home";
 import About from "./components/About";
 import Video from "./components/Video";
+import { Error } from "./components/Error";
 
 const App = () => {
-  const [linkId, setLinkId] = useState("");
-  const handleClick= (e) => {
-    setLinkId()
-  };
   return (
     <div className="App">
       <header className="App-header">
@@ -24,6 +20,7 @@ const App = () => {
             <Route exact path="/" element={<Home />} />
             <Route exact path="/about" element={<About />} />
             <Route exact path="/video/:id" element={<Video />} />
+            <Route path="*" element={<Error />} />
           </Routes>
         </div>
       </header>
---
+++ src/components/Error.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// Tutorial on creating a modal : https://dev.to/franciscomendes10866/how-to-create-a-modal-in-react-3coc
+
+export const Error = () => {
+  return (
+    <div>Error</div>
+  )
+}
