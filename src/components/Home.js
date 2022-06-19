05bf54cb4e8affdebbacb64a9a893e4bfc934de1
Tom LaTulipe <tlatulipe@gmail.com>
2022-06-19 19:49:58 -0400

    ERROR HANDLING >:(

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,6 @@ import VideoList from "./VideoList";
 import { Error } from "./Error";
 import { useState } from "react";
 
-
 import "./Home.css";
 
 const Home = () => {
@@ -16,7 +15,7 @@ const Home = () => {
     e.preventDefault();
 
     try {
-      if (!request) {
+      if (!request.length) {
         throw "Please enter a search request!";
       }
       fetch(
@@ -25,13 +24,20 @@ const Home = () => {
         .then((response) => response.json())
         .then((data) => {
           console.log(data);
+          if (data.items.length === 0) {
+            throw "No results found: Please try again!";
+          }
           setResults(data.items);
+        })
+        .catch((error) => {
+          console.log(error);
+          setShow(true);
+          setErr(error);
         });
     } catch (error) {
       console.log(error);
       setShow(true);
       setErr(error);
-
     }
     setRequest("");
   };
@@ -39,7 +45,7 @@ const Home = () => {
   return (
     <div>
       {/* <button onClick={() => setShow(true)}>Show modal</button> */}
-      <Error onClose={() => setShow(false)} show={show} error={err}/>
+      <Error onClose={() => setShow(false)} show={show} error={err} />
       <form onSubmit={handleSubmit} className="form">
         <label>
           <br />
@@ -53,7 +59,9 @@ const Home = () => {
           ></input>
         </label>
         <br />
-        <button type="submit" id='search-button'>Search</button>
+        <button type="submit" id="search-button">
+          Search
+        </button>
       </form>
       <div className="videolist">
         <VideoList results={results} />
