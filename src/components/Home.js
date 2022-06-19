43e93a1727bd2eb9f8e0e7089e4da33946581e6d
Tom LaTulipe <tlatulipe@gmail.com>
2022-06-19 14:11:01 -0400

    Attempting to handle empty search error

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,7 +12,11 @@ const Home = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    
     try {
+      if (!request) {
+        throw error;
+      }
       fetch(
         `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=9&q=${request}&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`
       )
