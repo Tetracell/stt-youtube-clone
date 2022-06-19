2ae034b159f4c5d158dcddf50a5eb3b63779ad18
Samantha Figueroa <96448061+samfigueroa92@users.noreply.github.com>
2022-06-19 15:34:55 -0400

    delete uneccessary code

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,6 +1,5 @@
 import React from "react";
-import styles from "./Error.css";
-import { RiCloseLine } from "react-icons/ri";
+import"./Error.css";
 
 // Tutorial on creating a modal : https://dev.to/franciscomendes10866/how-to-create-a-modal-in-react-3coc
 
