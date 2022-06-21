1c1754ab605d079262c14eedccd64fed21122fe9
Tom LaTulipe <tlatulipe@gmail.com>
2022-06-20 22:46:37 -0400

    Updated my about

--- src/components/About.js
+++ src/components/About.js
@@ -6,60 +6,147 @@ const samantha = require("../images/Samantha_Figueroa.jpg");
 const thierry = require("../images/Thierry_Ankoue.jpg");
 
 const About = () => {
-    return (
-        <div className="about">
-            <div>
-                <section className="project">
-                <h1>The Project</h1>
-                <p>Our Pursuit instructors tasked us with creating a clone of a popular video sharing website called YouTube. Using the JS, HTML, CSS and React knowledge we have gained thus far we were asked to create a React application that lets you search for YouTube videos using the YouTube API, then choose a video from a list to watch and comment on. We hope you enjoy our version of YouTube!</p>
-                </section>
-            </div>
-            <div className="fellows">
-            <section className="fellow-one">
-                <img src={thierry} alt=''></img>
-                <h3>Thierry Ankoue </h3>
-                <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
-                <a href="https://github.com/KingAnkoue">
-                <img src='https://cdn4.iconfinder.com/data/icons/geomicons/32/672344-github-512.png' id="github" alt='github-logo' className='link'/>
-                </a>
-                <a href="">
-                    <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/800px-LinkedIn_logo_initials.png' id="linkedin" alt='linkedin-logo' className='link'/>
-                </a>
-                <a href="mailto: ">
-                    <img src='https://www.kindpng.com/picc/m/588-5889138_cute-email-icon-png-transparent-png.png' id="email" alt='email' className='link'/>
-                </a>
-            </section>
-            <section className="fellow-two">
-                <img src={samantha} alt=''></img>
-                <h3>Samantha Figueroa</h3>
-                <p>Hi! My name is Sam and I am currently working with the World Day of Prayer International Committee as their Administrative & Communications Associate. I am Puerto Rican but have lived in NY my whole life. I love the city but enjoy visting my island as much as possible. I graduated Hunter College with a BA in Psychology but am now developing my web development skills in Javascript, HTML, CSS and React as a Pursuit fellow. In my free time I enjoy roller skating, playing video games, reading, and binge-watching my favorite tv shows or having move marathons.</p>
-                <a href="https://github.com/samfigueroa92">
-                    <img src='https://cdn4.iconfinder.com/data/icons/geomicons/32/672344-github-512.png' id="github" alt='github-logo' className='link'/>
-                </a>
-                <a href="https://www.linkedin.com/in/samantha-figueroa-b00336a4/">
-                    <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/800px-LinkedIn_logo_initials.png' id="linkedin" alt='linkedin-logo' className='link'/>
-                </a>
-                <a href="mailto: samanthafigueroa@pursuit.org">
-                    <img src='https://www.kindpng.com/picc/m/588-5889138_cute-email-icon-png-transparent-png.png' id="email" alt='email' className='link'/>
-                </a>
-            </section>
-            <section className="fellow-three">
-                <img src={tom} alt=""></img>
-                <h3>Tom LaTulipe</h3>
-                <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
-                <a href="https://github.com/Tetracell">
-                <img src='https://cdn4.iconfinder.com/data/icons/geomicons/32/672344-github-512.png' id="github" alt='github-logo' className='link'/>
-                </a> 
-                <a href="">
-                    <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/800px-LinkedIn_logo_initials.png' id="linkedin" alt='linkedin-logo' className='link'/>
-                </a>
-                <a href="mailto: ">
-                    <img src='https://www.kindpng.com/picc/m/588-5889138_cute-email-icon-png-transparent-png.png' id="email" alt='email' className='link'/>
-                </a>
-            </section>
-            </div>
-        </div>
-    )
+  return (
+    <div className="about">
+      <div>
+        <section className="project">
+          <h1>The Project</h1>
+          <p>
+            Our Pursuit instructors tasked us with creating a clone of a popular
+            video sharing website called YouTube. Using the JS, HTML, CSS and
+            React knowledge we have gained thus far we were asked to create a
+            React application that lets you search for YouTube videos using the
+            YouTube API, then choose a video from a list to watch and comment
+            on. We hope you enjoy our version of YouTube!
+          </p>
+        </section>
+      </div>
+      <div className="fellows">
+        <section className="fellow-one">
+          <img src={thierry} alt=""></img>
+          <h3>Thierry Ankoue </h3>
+          <p>
+            Pellentesque habitant morbi tristique senectus et netus et malesuada
+            fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae,
+            ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam
+            egestas semper. Aenean ultricies mi vitae est. Mauris placerat
+            eleifend leo. Pellentesque habitant morbi tristique senectus et
+            netus et malesuada fames ac turpis egestas. Vestibulum tortor quam,
+            feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu
+            libero sit amet quam egestas semper. Aenean ultricies mi vitae est.
+            Mauris placerat eleifend leo.
+          </p>
+          <a href="https://github.com/KingAnkoue">
+            <img
+              src="https://cdn4.iconfinder.com/data/icons/geomicons/32/672344-github-512.png"
+              id="github"
+              alt="github-logo"
+              className="link"
+            />
+          </a>
+          <a href="">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/800px-LinkedIn_logo_initials.png"
+              id="linkedin"
+              alt="linkedin-logo"
+              className="link"
+            />
+          </a>
+          <a href="mailto: ">
+            <img
+              src="https://www.kindpng.com/picc/m/588-5889138_cute-email-icon-png-transparent-png.png"
+              id="email"
+              alt="email"
+              className="link"
+            />
+          </a>
+        </section>
+        <section className="fellow-two">
+          <img src={samantha} alt=""></img>
+          <h3>Samantha Figueroa</h3>
+          <p>
+            Hi! My name is Sam and I am currently working with the World Day of
+            Prayer International Committee as their Administrative &
+            Communications Associate. I am Puerto Rican but have lived in NY my
+            whole life. I love the city but enjoy visting my island as much as
+            possible. I graduated Hunter College with a BA in Psychology but am
+            now developing my web development skills in Javascript, HTML, CSS
+            and React as a Pursuit fellow. In my free time I enjoy roller
+            skating, playing video games, reading, and binge-watching my
+            favorite tv shows or having move marathons.
+          </p>
+          <a href="https://github.com/samfigueroa92">
+            <img
+              src="https://cdn4.iconfinder.com/data/icons/geomicons/32/672344-github-512.png"
+              id="github"
+              alt="github-logo"
+              className="link"
+            />
+          </a>
+          <a href="https://www.linkedin.com/in/samantha-figueroa-b00336a4/">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/800px-LinkedIn_logo_initials.png"
+              id="linkedin"
+              alt="linkedin-logo"
+              className="link"
+            />
+          </a>
+          <a href="mailto: samanthafigueroa@pursuit.org">
+            <img
+              src="https://www.kindpng.com/picc/m/588-5889138_cute-email-icon-png-transparent-png.png"
+              id="email"
+              alt="email"
+              className="link"
+            />
+          </a>
+        </section>
+        <section className="fellow-three">
+          <img src={tom} alt=""></img>
+          <h3>Tom LaTulipe</h3>
+          <p>
+            My name is Tom. I currently work as a delivery coordinator for
+            Lowe's. I've lived in Connecticut my whole life, really only
+            bouncing between two towns. I have an associates degree in
+            Information Technology, and plan on going back to school to round
+            out my education in programming. Throughout my life I have dabbled
+            with programming in languages such as QBASIC, C++, C# and Python.
+            Joining Pursuit has allowed me to develop new skills in web
+            development, along with giving me a path to making use of them to
+            begin a new career. I am a serial video gamer, and my main 'outside
+            the house' hobby is playing competitive pinball in local leagues and
+            state / regional events. My brain is primarily filled with Star Trek
+            minutae from endlessly rewatchng episodes of the various series. I
+            once marathoned all extended editions of Lord of the Rings twice in
+            one day.
+          </p>
+          <a href="https://github.com/Tetracell">
+            <img
+              src="https://cdn4.iconfinder.com/data/icons/geomicons/32/672344-github-512.png"
+              id="github"
+              alt="github-logo"
+              className="link"
+            />
+          </a>
+          <a href="https://www.linkedin.com/in/tom-latulipe-a6835120/">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/800px-LinkedIn_logo_initials.png"
+              id="linkedin"
+              alt="linkedin-logo"
+              className="link"
+            />
+          </a>
+          <a href="mailto:tomlatulipe@pursuit.org ">
+            <img
+              src="https://www.kindpng.com/picc/m/588-5889138_cute-email-icon-png-transparent-png.png"
+              id="email"
+              alt="email"
+              className="link"
+            />
+          </a>
+        </section>
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,26 +1,30 @@
 import { Link } from "react-router-dom";
 import React from "react";
-import "./Nav.css"
+import "./Nav.css";
 
 const Nav = () => {
-  const home = require('../images/home.png');
+  const home = require("../images/home.png");
 
   return (
     <div className="navbar">
-        <img
+      <img
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/502px-Logo_of_YouTube_%282015-2017%29.svg.png"
-        id="ytlogo" />
-      <ul id='nav-list'>
+        id="ytlogo"
+      />
+      <ul id="nav-list">
         <li id="homelink">
           <Link to={"/"}>
             <img src={home} id="homelogo" />
             Home
-            </Link>
+          </Link>
         </li>
         <li id="aboutlink">
           <Link to={"/about"}>
-          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/Red_information_icon_with_gradient_background.svg/1024px-Red_information_icon_with_gradient_background.svg.png" id="abouticon" />
-          About
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/Red_information_icon_with_gradient_background.svg/1024px-Red_information_icon_with_gradient_background.svg.png"
+              id="abouticon"
+            />
+            About
           </Link>
         </li>
       </ul>
